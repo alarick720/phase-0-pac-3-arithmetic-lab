6e11428a29882d2e8561aa14695dd9af4d790dd5
Alarick Whitted <awhitted834@gmail.com>
2023-10-18 06:21:03 -0400

    done

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(add) {
+
+}
+function subtract(subtract){
+
+}
+function multiply(multiply){
+
+}
+function divide(divide){
+
+}
+function add(add){
+    return "something";
+}
+function add(a, b ){
+    return a + b; 
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+     return a / b;
+}function  increment(a,){
+     return ++a ;
+}
+function decrement(a){
+    return --a; 
+}
+ 
+
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+
+ 
+ 
+
+ 
+
+
